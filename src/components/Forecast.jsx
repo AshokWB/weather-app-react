f631cdc40001f1c6654f07598ae482ea8cde2cad
Ashok Thapa <ashokthapa698@gmail.com>
2022-12-09 16:13:25 +0545

    new code

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,24 +2,29 @@ import React from "react";
 import { iconUrlFromCode } from "../services/WeatherService";
 
 function Forecast({ title, items }) {
+  console.log(items);
   return (
     <div>
       <div className="flex items-center justify-start mt-6">
         <p className="text-white font-medium uppercase">{title}</p>
       </div>
       <hr className="my-2" />
+
       <div className="flex flex-row items-center justify-between text-white">
-        {items.map((item) => {
-          <div className="flex flex-col items-center justify-center">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center"
+          >
             <p className="font-light text-sm">{item.title}</p>
             <img
               src={iconUrlFromCode(item.icon)}
-              alt=""
               className="w-12 my-1"
+              alt=""
             />
-            <p className="font-medium">`${item.temp.toFixed()}°`</p>
-          </div>;
-        })}
+            <p className="font-medium">{`${item.temp.toFixed()}°`}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
